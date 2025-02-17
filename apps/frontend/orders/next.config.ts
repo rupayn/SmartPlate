6e0cd2c1e8bf67d5bb1ccc42229cc6cf84598264
Rupayan Nandi <rupayannandi8055@gmail.com>
2025-02-17 19:14:12 +0530

    json update

--- apps/frontend/orders/next.config.ts
+++ apps/frontend/orders/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "orders",
+        filename: "static/chunks/remoteEntry.js",
+        extraOptions: {},
+        exposes: {
+          "./OrderList": "./components/OrderList.tsx",
+          "./OrderDetails": "./components/OrderDetails.tsx",
+        },
+        shared: {},
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
