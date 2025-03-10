53fdc76176b0064c52d7c685dcad1ab2437d9ccd
Rupayan Nandi <rupayannandi8055@gmail.com>
2025-03-10 12:13:13 +0530

    cookies added in signup

--- apps/frontend/auth/src/app/api/signup/route.ts
+++ apps/frontend/auth/src/app/api/signup/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@repo/db/config";
 import {  NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
+import { cookies } from "next/headers";
+import { jwtEncryptUser, objectFilter, userInterface } from "@repo/common/config";
 export function GET(){
     return NextResponse.json({
         success: true,
@@ -39,12 +41,30 @@ export async function POST(req:NextRequest){
         enrollment,
         email,
         password,
-        avatar: "asdfghj",
+        avatar: `https://api.dicebear.com/7.x/identicon/svg?seed=${name}`,
       },
     });
+    
+    const userWithoutPass:userInterface =await objectFilter(userd, "password");
+
+    const encryptUser:string = await jwtEncryptUser(
+              userWithoutPass,
+              String(process.env.NEXTAUTH_SECRET)
+            );
+
+    const cookieStore=await cookies();
+            cookieStore.set({
+              name: "next-auth-token",
+              value: encryptUser,
+              httpOnly: true,
+              path: "/",
+              sameSite: "lax",
+              expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
+              domain: "localhost",
+            });
 
     return NextResponse.json({
-        success: true,
-        userd
-    })
+      success: true,
+      userWithoutPass,  
+    });
 }
